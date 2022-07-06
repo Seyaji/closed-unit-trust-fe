796b684125c93fbfc6bae4d253f482c95db01ff2
Alex <alexchelseaalex@gmail.com>
2022-07-06 02:59:04 +0100

    refactor global styles, reorganise files

--- src/lib/styles/global.ts
+++ src/lib/styles/global.ts
@@ -1,20 +1,31 @@
 import { css } from "@emotion/react";
 
-export const GlobalStyles = css`
+// @ts-ignore
+export const GlobalStyles = (theme) => `
   * {
     box-sizing: border-box;
   }
-  :root {
-    margin: 0;
-    padding: 0;
-  }
-  body {
+  #root {
     margin: 0;
     padding: 0;
   }
 
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: ${theme.colors.background};
+    color: ${theme.colors.text};
+    max-width: 100%;
+    overflow-x: hidden;
+  }
+
   h1, h2, h3, h4, h5, h6, button {
     font-family: 'Roboto', sans-serif;
+    color: ${theme.colors.bold};
+  }
+
+  p {
+    font-size: 1rem;
   }
 
   div {
