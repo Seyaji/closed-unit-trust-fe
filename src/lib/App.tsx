f237022b4c30346d303e6e00b3d1212eba53d5a8
Alex <alexchelseaalex@gmail.com>
2022-07-06 16:10:35 +0100

    implement browser routing for home and account page

--- src/lib/App.tsx
+++ src/lib/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import { Routes, Route, BrowserRouter, } from "react-router-dom";
 import { ThemeProvider, Theme } from '@emotion/react'
 import { Global } from "@emotion/react";
 import { GlobalStyles } from './styles/global'
 import { darkTheme, lightTheme } from './styles/theme'
 
 import { Nav } from './components/Nav';
-import Page from './styles/Page';
 import HomePage from './pages/HomePage';
 import InvestorPage from './pages/Investor';
-import Contract from './components/contract/Contract';
 
 
 export default function App() {
@@ -16,12 +15,14 @@ export default function App() {
   const theme = currentTheme === darkTheme ? darkTheme : lightTheme;
   return (
     <ThemeProvider theme={theme}>
-      <Nav />
       <Global styles={theme => GlobalStyles} />
-        <InvestorPage />
-        <Page>
-          <Contract />
-        </Page>
+      <BrowserRouter>
+      <Nav />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/investor" element={<InvestorPage />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
